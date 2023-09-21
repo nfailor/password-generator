f3bcc1966c22707df800c7d6e16703e0334f6bed
nfailor <nfailor@gmail.com>
2023-09-20 20:51:04 -0500

    Applied password length prompt and confirmation user prompts to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,32 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function generatePassword() {
-    var passLength = prompt("Enter password length from 8 to 128 characters");
+    var passLength = parseInt(prompt("Enter password length from 8 to 128 characters"));
+    
+    if (passLength < 8 || passLength > 128) {
+        alert("Invalid Length")
+        return;
+    }
+
+    alert("A successful password includes lowercase letters, uppercase letters, numeric characters, and special characters")
+
+    var lCase = confirm("Would you like to include lowercase characters?")
+    var uCase = confirm("Would you like to include uppercase characters?")
+    var iNum = confirm("Would you like to include numeric characters?")
+    var iSpec = confirm("Would you like to include special characters?")
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 function writePassword() {
