5eae4699353b20da7f32149e4801f9bd9af8b734
nfailor <nfailor@gmail.com>
2023-09-25 15:34:05 -0500

    Added comments to script.js file and updated README.md file with proper usage and screenshots

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Nicklaus Failor Password Generator
+# Password Generator
 
 ## NAME
 The name of this webpage is Password Generator.
@@ -13,7 +13,25 @@ I used HTML and CSS to create the contents of this webpage and javascript to cre
 This is a website only and does not need installation. it is currently hosted in github and you can access it at Github; https://nfailor.github.io/password-generator/
 
 ## USAGE
-This webpage is used to generate random passwords.
+This webpage is used to generate random passwords. See webpage below:
+
+![alt text](assets/images/pwgen-hp.JPG)
+
+In order to generate your new password, please click "Generate Password" and see below prompt:
+
+![alt text](assets/images/pwgen-p1.JPG)
+
+Click okay, and it will then prompt the user to select a password length between 8 and 128 characters:
+
+![alt text](assets/images/pwgen-p1.5.JPG)
+
+Enter a number between 8 and 128 to be sent to the next prompt:
+
+![alt text](assets/images/pwgen-p2.JPG)
+
+If the user would like to include lowercase characters, they would select 'Ok.' If they would like to exclude lowercase characters, just select 'Cancel.' Upon making their choice, the user will find three similar prompts, addresses uppercase characters, numbers, and special characters. After all selections have been made, the prompt window will disappear, and the user's new password will be displayed inside the dash-lined box, as seen below:
+
+![alt text](assets/images/pwgen-fin.JPG)
 
 ## SUPPORT AND CONTRIBUTION
 If you have any questions or concerns, please reach out to nfailor@gmail.com
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,28 @@
-// Assignment Code
+// this variable is used to create the "generate password" button on the webpage
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// this function is what is used in order to prompt the user on their desired password specifications
 function generatePassword() {
+    // this alert is used to send a message to the user during the prompt
     alert("A strong password includes lowercase letters, uppercase letters, numeric characters, and special characters");
-    
+    // the passLength variable is used to properly use the below 'if' statement
     var passLength = parseInt(prompt("Enter password length from 8 to 128 characters"));
-    
+    // this 'if' statement is used so that if the user provides an input that is not a number between 8 and 128, it will return as invalid
     if (passLength < 8 || passLength > 128) {
         alert("Invalid Length")
         return;
     };
-
+    // these below variables and confirmation forms are used to prompt the user on the specific requirements of their password
     var lCase = confirm("Would you like to include lowercase characters?");
     var uCase = confirm("Would you like to include uppercase characters?");
     var iNum = confirm("Would you like to include numeric characters?");
     var iSpec = confirm("Would you like to include special characters?");
-
+    // in order to promote strong password usage, if all are not selected, the user will be shown the following alert message
     if (!lCase || !uCase || !iNum || !iSpec) {
         alert("Please accept all choices to generate a strong password");
         return;
     };
-
+    // these variables are used to give the below 'for' loop the proper elements to work, specifically to provide a randomized password with the following character possibilities
     var lowerCase = "abcdefghijklmnopqrstuvwxyz";
     var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var inputNum = "0123456789";
@@ -39,7 +40,7 @@ function generatePassword() {
     return generatedPassword;
 
 };
-
+// the below function is used in order to run the password generation prompt and then properly display the freshly generated randomized password on the webpage
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -48,5 +49,5 @@ function writePassword() {
 
 };
 
-// Add event listener to generate button
+// the below event listener is used to make the "Generate Password" button on the webpage clickable, and upon clicking it, will activate the 'writePassword' function in line 44
 generateBtn.addEventListener("click", writePassword);
